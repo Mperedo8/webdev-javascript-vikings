20be530f22d706473d01360fee6bbc553af22449
Peredo Matthew <20256@columbushs.com>
2019-12-09 13:44:59 -0500

    Good start

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,8 +1,29 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor (health, strength){
+        this.health = health;
+        this.strength = strength;
+    };
+    
+    attack() {
+    return this.strength
+    };
 
+    receiveDamage(theDamage) {
+       this.health -= theDamage;
+    };
+}
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor (name, health, strength){
+        super (health, strength);
+        this.name = name;
+
+        if (this.health > 0)
+        return `$(this.name) has received $(theDamage) points of damage`
+    }
+    else ()
+}
 
 // Saxon
 class Saxon {}
